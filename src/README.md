e9cc20e54e3070264cd496157dd0fc9587963769
Peter <peterdalle@gmail.com>
2018-08-11 11:11:21 +0200

    src tools, names in CSV

---
+++ src/README.md
@@ -0,0 +1,6 @@
+# src
+
+This directory contains Python source code to convert the original files to different formats:
+
+- <name-json-to-csv.py>: Convert names in separate JSON files (kvinnonamn.json and mansnamn.json) to separate CSV files.
+- <name-json-to-single-csv.py>: Convert names in separate JSON files to a single CSV file with `name` and `gender` headers.
---
+++ src/name-json-to-single-csv.py
@@ -0,0 +1,31 @@
+import json
+import csv
+
+print("Converting...")
+
+# Read JSON to list.
+f = open("kvinnonamn.json", "r", encoding="utf8")
+females = json.load(f)
+f.close()
+
+# Read JSON to list.
+f = open("mansnamn.json", "r", encoding="utf8")
+males = json.load(f)
+f.close()
+
+# Write list to CSV with "name,gender" headers.
+f = open("namn.csv", "w")
+f.write("name,gender")
+
+# Write females.
+for name in females:
+	f.write("\n")
+	f.write("{0},{1}".format(name, "female"))
+	
+# Write males.
+for name in males:
+	f.write("\n")
+	f.write("{0},{1}".format(name, "male"))
+	
+f.close()
+print("Done.")
